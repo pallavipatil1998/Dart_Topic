a2fcea844365573549cc5aa0255449128b0a278e
Pallavi Patil <pallavishravan545@gmail.com>
2023-08-05 18:10:53 +0530

    T4 Inheritance Consept

---
+++ lib/inheritance_t5.dart
@@ -0,0 +1,49 @@
+void main(){
+  B b=B(); //single
+  b.funcA();
+  b.funcB();
+
+  C c= C(); //MultiLevel Inheritance
+  c.funcA();
+  c.funcB();
+  c.funcC();
+
+  D d=D();   //Hirachical Inheritance also b.funcA(); & c.funcA();
+  d.funcA();
+  d.funcD();
+  E e=E();
+  e.funcA();
+  e.funcE();
+
+}
+
+class A{
+  funcA(){
+    print('A');
+  }
+}
+
+class B extends A{
+  funcB(){
+    print('B');
+  }
+}
+
+class C extends B{
+  funcC(){
+    print("C");
+  }
+}
+
+
+class D extends A{
+  funcD(){
+    print('D');
+  }
+}
+
+class E extends A{
+  funcE(){
+    print("E");
+  }
+}
